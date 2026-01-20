d339ce92a160a05b39b7f44f74d946217caa1982
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2026-01-20 09:57:53 -0600

    Add Vercel adapter and config for Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
   site: 'https://launch.sow.care',
   output: 'static',
+  adapter: vercel(),
 });
